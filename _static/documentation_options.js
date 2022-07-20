e0fa7e053f1c5fc196da8a58221c8ce996e8a221
Circle CI <circle@benchopt.com>
2022-07-20 21:55:50 +0000

    CircleCI update of dev docs (1873).

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.1.dev55',
+    VERSION: '1.2.1.dev56',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
