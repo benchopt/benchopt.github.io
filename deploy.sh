f8e679032ccd3f93658bc2813a7d971e90f902fe
tommoral <thomas.moreau.2010@gmail.com>
2025-09-25 09:02:47 +0200

    Update docs for 1.7.0 (build )

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+
+DEST=$1
+
+# copying the current doc to its destination
+rm -rf "$DEST"
+cp -r ../doc/_build/html/ "$DEST";
+if [ -n "$CIRCLE_TAG" ]; then
+                # Also update stable -> copy into "stable/"
+                rm -rf stable
+                cp -r "$DEST" stable
+fi
+
+# Creating the version dropdown
+VERSIONS=$(ls -d v* | sort -Vr | jq -R -s -c 'split("\n")[:-1]')
+cat > versions.json <<EOF
+{
+              "stable": "stable",
+              "dev": "dev",
+              "versions": $VERSIONS
+}
+EOF
+
+echo "Deploying dev docs for ${CIRCLE_BRANCH}.";
+git add -A
+if [[ -z $(git diff --staged) ]]; then
+              echo "Nothing to commit"
+else
+              MSG="Update docs for $DEST (build ${CIRCLE_BUILD_NUM})"
+              git commit -m "$MSG";
+              git push origin main;
+fi
+
